a1cc8bd9d47a404701fba544755dfe0f1d6c0b8b
David <david.peel.ja@gmail.com>
2022-07-10 18:14:12 +1000

    adds dir clean up to model

--- src/data-processor/collect-f1.sh
+++ src/data-processor/collect-f1.sh
@@ -1,4 +1,7 @@
+rm -r ./src/model/raw
+mkdir ./src/model/raw
 curl -o ./src/model/raw/f1-schedule.json --location --request GET 'https://ergast.com/api/f1/current.json'
 curl -o ./src/model/raw/f1-wdc-standings.json --location --request GET 'https://ergast.com/api/f1/current/driverStandings.json'
 curl -o ./src/model/raw/f1-wcc-standings.json --location --request GET 'https://ergast.com/api/f1/current/constructorStandings.json'
 node ./src/data-processor/extract.js
+rm -r ./src/model/raw
